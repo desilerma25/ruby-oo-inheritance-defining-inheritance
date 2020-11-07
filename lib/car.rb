b60f24237f8b5ca9ad5ed07d13000becf1dcf1ee
Desiree Lerma <desi.lerma96@yahoo.com>
2020-11-07 16:57:54 -0600

    commit tree e3e4e6b30904a9bdd8458d7aa6118d0e2e116e1f

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,13 @@
 require_relative "./vehicle.rb"
+# subclass / child class
+# req. our vehicle file b/c our class will need to access Vehicle
+
+class Car < Vehicle
+    # using <, means Car in inheriting Vehicle
+
+    def go
+        "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    end
+    # this overwrites the inherited method w/ a specific one for Car
+
+end
